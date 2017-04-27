25e289b200d371f02cf37d8647ce194f9505e298
Shamari Southwell <shamari.southwell@live.com>
2017-04-27 16:06:03 -0400

    Add files via upload
    Added login page for users & new users. Just UI - has no functionality.

---
+++ login.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Tech Deals</title>
+
+    <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="loginstyles.css">
+  </head>
+  <body>
+
+    <div class="wrapper">
+      <form class="form-signin">
+        <h2 class="form-signin-heading">Sign In</h2>
+        <label for="username" id="text-label">Email</label>
+        <input type="text" class="form-control" name="username" placeholder="Email Address" required="" autofocus="" />
+        <label for="password" id="text-label">Password</label>
+        <input type="password" class="form-control" name="password" placeholder="Password" required=""/>
+
+        <button class="btn btn-lg btn-primary btn-block" type="submit">Login</button>
+
+          <div class="login">
+            New to Tech Deals? <a href="newlogin.html">Sign Up</a>
+          </div>
+
+
+      </form>
+
+
+    </div>
+
+
+
+  </body>
+</html>
---
+++ newlogin.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Tech Deals</title>
+
+    <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+<link rel="stylesheet" href="loginstyles.css">
+  </head>
+
+    <div class="wrapper">
+
+      <form class="form-signin" action="index.html" method="post">
+        <h2 class="form-signin-heading">Sign Up</h2>
+        <label for="firstName" id="text-label">First Name</label>
+        <input type="text" class="form-control" name="firstName" placeholder="First Name" required="" autofocus="" />
+        <label for="lastName" id="text-label">Last Name</label>
+        <input type="text" class="form-control" name="lastName" placeholder="Last Name" required="" autofocus="" />
+        <label for="username" id="text-label">Email</label>
+        <input type="text" class="form-control" name="username" placeholder="Email Address" required="" autofocus="" />
+        <label for="password" id="text-label">Password</label>
+        <input type="password" class="form-control" name="password" placeholder="Password" required=""/>
+
+          <button class="btn btn-lg btn-primary btn-block" type="submit">Login</button>
+
+          <div class="login">
+            Already have an account? <a href="login.html">Sign in</a>
+          </div>
+
+      </form>
+
+    </div>
+
+
+  <body>
+
+  </body>
+</html>
